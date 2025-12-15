9f9de77c4a64396060ba44de6c52762ba2ababd6
iyarsius <iyarsius@gmail.com>
2025-12-15 23:50:00 +0100

    Updated trace actions client type to match exactly to viem

--- src/actions/traceFilter.ts
+++ src/actions/traceFilter.ts
@@ -1,4 +1,5 @@
-import  type { TraceFilterParameters, TraceFilterReturnType, CoreClient, Hash } from "../types";
+import type { Client, Transport, Chain } from "viem";
+import type { TraceFilterParameters, TraceFilterReturnType } from "../types";
 
 /**
  * Calls the 'trace_filter' RPC method to retrieve execution traces
@@ -7,25 +8,30 @@ import  type { TraceFilterParameters, TraceFilterReturnType, CoreClient, Hash } 
  * This function sends a JSON-RPC request to the Ethereum node (or EVM-compatible)
  * via the provided client object and returns the array of trace entries matching
  * the filters.
- * 
+ *
  * NOTE: This method relies on the non-standard 'trace_filter' method of the RPC.
  * It may not be supported by all nodes.
  *
- * @param client The CoreClient instance used to perform the RPC call.
+ * @param client The client instance used to perform the RPC call.
  * @param params The filtering parameters for the trace request (block range, addresses, etc.).
  * @returns A promise that returns an array of trace entries (GenericTraceEntry[])
  * that match the filtering criteria.
  */
-export async function traceFilter(client: CoreClient, params: TraceFilterParameters): Promise<TraceFilterReturnType> {
-    if (typeof params.fromAddress === "string") params.fromAddress = [params.fromAddress];
-    if (typeof params.toAddress === "string") params.toAddress = [params.toAddress];
-    
-    return await client.request<{
-        method: 'trace_filter',
-        Parameters: TraceFilterParameters[],
-        ReturnType: TraceFilterReturnType
-    }>({
-        method: 'trace_filter',
-        params: [params],
-    });
+export async function traceFilter<TChain extends Chain | undefined>(
+  client: Client<Transport, TChain>,
+  params: TraceFilterParameters,
+): Promise<TraceFilterReturnType> {
+  if (typeof params.fromAddress === "string")
+    params.fromAddress = [params.fromAddress];
+  if (typeof params.toAddress === "string")
+    params.toAddress = [params.toAddress];
+
+  return await client.request<{
+    method: "trace_filter";
+    Parameters: TraceFilterParameters[];
+    ReturnType: TraceFilterReturnType;
+  }>({
+    method: "trace_filter",
+    params: [params],
+  });
 }
--- src/actions/traceTransaction.ts
+++ src/actions/traceTransaction.ts
@@ -1,4 +1,8 @@
-import  type { TraceTransactionParameters, TraceTransactionReturnType, CoreClient, Hash } from "../types";
+import type { Client, Transport, Chain } from "viem";
+import type {
+  TraceTransactionParameters,
+  TraceTransactionReturnType,
+} from "../types";
 
 /**
  * Calls the 'trace_transaction' RPC method to retrieve execution traces
@@ -7,22 +11,25 @@ import  type { TraceTransactionParameters, TraceTransactionReturnType, CoreClien
  * This function sends a JSON-RPC request to the Ethereum node (or EVM-compatible)
  * via the provided client object and returns the array of trace entries for
  * the specified transaction hash.
- * 
+ *
  * NOTE: This method relies on the non-standard 'trace_transaction' method of the RPC.
  * It may not be supported by all nodes.
- * 
- * @param client The CoreClient instance used to perform the RPC call.
+ *
+ * @param client The client instance used to perform the RPC call.
  * @param params The transaction hash for which to retrieve traces.
  * @returns A promise that returns an array of trace entries (GenericTraceEntry[])
  * for the specified transaction hash.
  */
-export async function traceTransaction(client: CoreClient, params: TraceTransactionParameters): Promise<TraceTransactionReturnType> {
-    return await client.request<{
-        method: 'trace_transaction',
-        Parameters: TraceTransactionParameters[],
-        ReturnType: TraceTransactionReturnType
-    }>({
-        method: 'trace_transaction',
-        params: [params],
-    });
+export async function traceTransaction<TChain extends Chain | undefined>(
+  client: Client<Transport, TChain>,
+  params: TraceTransactionParameters,
+): Promise<TraceTransactionReturnType> {
+  return await client.request<{
+    method: "trace_transaction";
+    Parameters: TraceTransactionParameters[];
+    ReturnType: TraceTransactionReturnType;
+  }>({
+    method: "trace_transaction",
+    params: [params],
+  });
 }
