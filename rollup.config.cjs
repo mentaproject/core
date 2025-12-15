dc713ccd11f2a6939294b8ecda728592527a615f
iyarsius <iyarsius@gmail.com>
2025-12-15 22:37:20 +0100

    Fix build error with zerodev package

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,16 +1,16 @@
-const typescript = require('@rollup/plugin-typescript');
-const resolve = require('@rollup/plugin-node-resolve');
-const commonjs = require('@rollup/plugin-commonjs');
-const json = require('@rollup/plugin-json');
+const typescript = require("@rollup/plugin-typescript");
+const resolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const json = require("@rollup/plugin-json");
 
 const entries = [
-  { input: 'src/index.ts', output: 'dist/index' },
-  { input: 'src/actions/index.ts', output: 'dist/actions/index' },
-  { input: 'src/accounts/index.ts', output: 'dist/accounts/index' },
-  { input: 'src/types/index.ts', output: 'dist/types/index' },
-  { input: 'src/chains/index.ts', output: 'dist/chains/index' },
-  { input: 'src/clients/index.ts', output: 'dist/clients/index' },
-  { input: 'src/utils/index.ts', output: 'dist/utils/index' },
+  { input: "src/index.ts", output: "dist/index" },
+  { input: "src/actions/index.ts", output: "dist/actions/index" },
+  { input: "src/accounts/index.ts", output: "dist/accounts/index" },
+  { input: "src/types/index.ts", output: "dist/types/index" },
+  { input: "src/chains/index.ts", output: "dist/chains/index" },
+  { input: "src/clients/index.ts", output: "dist/clients/index" },
+  { input: "src/utils/index.ts", output: "dist/utils/index" },
 ];
 
 module.exports = entries.map(({ input, output }) => ({
@@ -18,12 +18,12 @@ module.exports = entries.map(({ input, output }) => ({
   output: [
     {
       file: `${output}.js`,
-      format: 'cjs',
+      format: "cjs",
       sourcemap: true,
     },
     {
       file: `${output}.mjs`,
-      format: 'es',
+      format: "es",
       sourcemap: true,
     },
   ],
@@ -32,14 +32,9 @@ module.exports = entries.map(({ input, output }) => ({
     commonjs(),
     json(),
     typescript({
-      tsconfig: './tsconfig.json',
+      tsconfig: "./tsconfig.json",
       declaration: false,
     }),
   ],
-  external: [
-    'permissionless',
-    'viem',
-    /^permissionless\/.*/,
-    /^viem\/.*/,
-  ],
+  external: ["permissionless", "viem", /^permissionless\/.*/, /^viem\/.*/],
 }));
